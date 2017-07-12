743f72338cb89009a60f930d223b23a131ce1548
Wanyue <RosettaWYzhang@users.noreply.github.com>
2017-07-12 09:19:33 +0800

    new questions

---
+++ src/chap2/Q2.java
@@ -0,0 +1,10 @@
+package chap2;
+
+/**
+ * Created by wanyuezhang on 12/07/2017.
+ * Implement an algorithm to find the kth to last element of a singly linked list
+ */
+public class Q2 {
+    
+
+}
